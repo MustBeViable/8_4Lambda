207ed2ea3f390bf811264c6a2aa745f6b9923a69
Elias <elias1rinne@gmail.com>
2025-10-05 20:21:38 +0300

    Task 2 done

--- src/main/java/Task2/LambdaList.java
+++ src/main/java/Task2/LambdaList.java
@@ -1,6 +1,7 @@
 package Task2;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class LambdaList {
     public static void main(String[] args) {
@@ -12,21 +13,41 @@ public class LambdaList {
         ArrayList<Integer> intOdd = new ArrayList<>(intList);
         ArrayList<Integer> intEven = new ArrayList<>(intList);
         ArrayList<Integer> doubledInts = new ArrayList<>(intList);
+        ArrayList<Integer> sumOfInts = new ArrayList<>(intList);
 
 
         System.out.println("Alkuperänen lisna: " + intList);
         System.out.println();
 
         intOdd.removeIf(n -> n % 2 == 0);
-        System.out.println("Parittomat: " + intOdd);
+        intOdd.replaceAll(n -> {
+            if (n % 2 != 0) {
+                return n * 2;
+            }
+            return n;
+        } );
+        System.out.println("Parittomat kerrottuna kahdella: " + intOdd);
         System.out.println();
+
+
+/*
+
+ */
+
 
         intEven.removeIf(n -> n%2 != 0);
         System.out.println("Parilliset: " + intEven);
         System.out.println();
 
+
+
         doubledInts.replaceAll(n -> n*2);
         System.out.println("Tuplauksen jälkeen: " + doubledInts);
 
+        final int[] sumBox = {0};
+        sumOfInts.forEach(n -> sumBox[0] += n);
+        int sumA = sumBox[0];
+        System.out.println("Summa: " + sumA);
         }
+
     }
